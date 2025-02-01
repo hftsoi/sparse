ed4bda9a5c6aa1ceefb9d77ef42bb5bca4f216fb
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-02-01 21:00:28 +0000

    comparison

---
+++ model_conv20/model_test_prj/solution1/.autopilot/db/kernel_module_hierarchy.tcl
@@ -0,0 +1,7 @@
+set ModuleHierarchy {[{
+"Name" : "model_test","ID" : "0","Type" : "dataflow",
+"SubInsts" : [
+	{"Name" : "conv_2d_cl_ap_fixed_12_4_0_0_0_ap_fixed_25_10_5_3_0_config2_U0","ID" : "1","Type" : "sequential",
+		"SubLoops" : [
+		{"Name" : "PartitionLoop","ID" : "2","Type" : "pipeline"},]},]
+}]}
