ee7d605a59920dade309728629e3c04a305efae1
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-04-01 19:18:53 +0000

    part number

--- model_conv_parallel_pf400/project.tcl
+++ model_conv_parallel_pf400/project.tcl
@@ -3,7 +3,7 @@ set project_name "model_test"
 variable backend
 set backend "vivado"
 variable part
-set part "xcvu13p-flga2577-2-e"
+set part "xcu250-figd2104-2L-e"
 variable clock_period
 set clock_period 5
 variable clock_uncertainty
