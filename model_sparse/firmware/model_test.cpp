ef90f11059dd413c74ccae019d118b436ecfd3fd
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-04-07 20:45:33 +0000

    update sparse layers with comments

--- model_sparse/firmware/model_test.cpp
+++ model_sparse/firmware/model_test.cpp
@@ -20,10 +20,10 @@ struct value_idx_pair {
 
 template <class T, int N, class Op> T find_nonzero(T *x, Op op) {
 #pragma HLS INLINE
-    // this function finds the leftmost nonzero element in an array
+    // This function finds the leftmost nonzero element in an array.
 
-    // split the array with two subarrays
-    // leftN: largest power of 2 that is less than N
+    // Split the array with two subarrays.
+    // leftN: largest power of 2 that is less than N.
     static constexpr int leftN = _pow2(_floorlog2(N - 1)) > 0 ? _pow2(_floorlog2(N - 1)) : 0;
     static constexpr int rightN = N - leftN > 0 ? N - leftN : 0;
 
@@ -41,16 +41,21 @@ template <class data_T, class hash_T, int N_h, int N_w, int N_c, int N_sparse>
 void sparse_input_reduce(data_T input_arr[N_h * N_w * N_c],
                          data_T sparse_arr_feat[N_sparse * N_c],
                          hash_T sparse_arr_hash[N_sparse * 2]) {
+    // This function reduces the full input array into sparse arrays, storing active inputs (nonzeros).
+    // 1) sparse_arr_feat stores features (N_c channels) from the active pixels.
+    // 2) sparse_arr_hash stores the corresponding pixel height and width indices, in the same ordering as sparse_arr_feat.
+    // The sparse arrays assume a fixed max number of active pixels considered in a dataset (N_sparse).
+    // For simplicity, scan for active pixels in the first channel only.
+    // If a pixel is active in the first channel, then store the features from all channels.
 
     value_idx_pair<data_T> pair_arr[N_h * N_w];
-    #pragma HLS ARRAY_PARTITION variable=pair_arr type=complete dim=0
-
-    // scan the first channel only, and assume the same sparsity structure for the rest of channels
     int j_h_arr[N_h * N_w];
     int j_w_arr[N_h * N_w];
     #pragma HLS ARRAY_PARTITION variable=j_h_arr type=complete dim=0
     #pragma HLS ARRAY_PARTITION variable=j_w_arr type=complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=pair_arr type=complete dim=0
 
+    // Loop over pixels and get features from the first channel, also compute height and width indices.
     DataPrepareLoop:
     for (int j = 0; j < N_h * N_w; j++) {
         #pragma HLS UNROLL
@@ -69,48 +74,66 @@ void sparse_input_reduce(data_T input_arr[N_h * N_w * N_c],
         // but resources stayed exactly the same
     }
 
+    // Find active pixels and fill the sparse arrays.
     Op_nonzero<value_idx_pair<data_T>> op_nonzero;
     MaxPixelsLoop:
     for (int i = 0; i < N_sparse; i++) {
         #pragma HLS PIPELINE
+        // Iteratively find the leftmost nonzero element in the input array.
         value_idx_pair<data_T> pair = find_nonzero<value_idx_pair<data_T>,
                                                    N_h * N_w,
                                                    Op_nonzero<value_idx_pair<data_T>>>(pair_arr, op_nonzero);
-        // first channel
+        // Fill feature for the first channel.
         sparse_arr_feat[N_c * i] = pair.value;
-        // other channels
+        // Other channels, if there is any.
         for (int j = 1; j < N_c; j++) {
             #pragma HLS UNROLL
             sparse_arr_feat[N_c * i + j] = input_arr[N_c * pair.index + j];
         }
 
+        // Fill height and width indices.
         sparse_arr_hash[2 * i] = j_h_arr[pair.index]; 
         sparse_arr_hash[2 * i + 1] = j_w_arr[pair.index];
 
+        // Update the filled nonzero feature to zero so the next find_nonzero can find the next nonzero.
         pair_arr[pair.index].value = 0;
     }
+    // Note that sparse_arr_hash[2 * i] and sparse_arr_hash[2 * i + 1] can fill arbitrary numbers even when the corresponding
+    // sparse_arr_feat[N_c * i], sparse_arr_feat[N_c * i + 1], ..., sparse_arr_feat[N_c * i + N_c - 1] are zeros (inactive).
+    // This will be the case when the number of active pixels is less then N_sparse in an example.
+    // These "zero-padded" pixels will be ignored as the following sparse operations will only be effective on nonzero elements in sparse_arr_feat.
 }
 
 template <class data_T, class res_T, class w_T, int n_chan, int n_filt, int N_sparse>
-res_T mult_for_sparse_conv(int offset_h, int offset_w, data_T sparse_arr_feat_in[n_chan * N_sparse], w_T filt_w[3 * 3 * n_chan * n_filt], int i_filt, int i_in) {
-    #pragma HLS INLINE
+res_T mult_for_sparse_conv_kernel3(int offset_h, int offset_w, data_T sparse_arr_feat_in[n_chan * N_sparse], w_T filt_w[3 * 3 * n_chan * n_filt], int i_filt, int i_pixel_in) {
+    // Helper function for sparse_conv, for a kernel size of 3.
+    // It picks the correct filter weights when multiplying the input features over channels, given a filter position and a pixel position.
 
+    #pragma HLS INLINE
     res_T acc = 0;
+    MultLoopPerFilter:
     for (int i_chan = 0; i_chan < n_chan; i_chan++) {
         #pragma HLS UNROLL
         w_T w = 0;
+        // Note the ordering in weight array: [p1-f1-c1, p1-f1-c2, p1-f2-c1, p1-f2-c2, p2-f1-c1,...],
+        // if there are two input channels and two output filters.
+        // Here w_idx computes the relative weight index for a given filter and channel, disregarding the pixel offset position.
         int w_idx = n_chan * i_filt + i_chan;
-        if ((offset_h == 1) && (offset_w == 1))        { w = filt_w[w_idx]; }
+
+        // Now figure out the pixel offset position given the offset values in height and width.
+        // Here offset_h (offset_w) is the relative position between the output and input pixel locations in height (width).
+        if ((offset_h == 1) && (offset_w == 1))        { w = filt_w[w_idx]; } // Top left filter weight.
         else if ((offset_h == 1) && (offset_w == 0))   { w = filt_w[w_idx + n_filt * n_chan]; }
-        else if ((offset_h == 1) && (offset_w == -1))  { w = filt_w[w_idx + n_filt * n_chan * 2]; }
+        else if ((offset_h == 1) && (offset_w == -1))  { w = filt_w[w_idx + n_filt * n_chan * 2]; } // Top right filter weight.
         else if ((offset_h == 0) && (offset_w == 1))   { w = filt_w[w_idx + n_filt * n_chan * 3]; }
-        // the central one has been done outside this, as it needs no offset check, and some LUTs are saved
+        // The central one has been done outside this, as it is always multiplied so needs no extra offset check, hence saving some LUTs.
         else if ((offset_h == 0) && (offset_w == -1))  { w = filt_w[w_idx + n_filt * n_chan * 5]; }
-        else if ((offset_h == -1) && (offset_w == 1))  { w = filt_w[w_idx + n_filt * n_chan * 6]; }
+        else if ((offset_h == -1) && (offset_w == 1))  { w = filt_w[w_idx + n_filt * n_chan * 6]; } // Bottom left filter weight.
         else if ((offset_h == -1) && (offset_w == 0))  { w = filt_w[w_idx + n_filt * n_chan * 7]; }
-        else if ((offset_h == -1) && (offset_w == -1)) { w = filt_w[w_idx + n_filt * n_chan * 8]; }
+        else if ((offset_h == -1) && (offset_w == -1)) { w = filt_w[w_idx + n_filt * n_chan * 8]; } // Bottom right filter weight.
         
-        acc += w * sparse_arr_feat_in[n_chan * i_in + i_chan];
+        // Dot product between the feature vector at a given input pixel and the corresponding weight vector, for a given filter.
+        acc += w * sparse_arr_feat_in[n_chan * i_pixel_in + i_chan];
     }
     return acc;
 }
@@ -121,38 +144,48 @@ void sparse_conv(data_T sparse_arr_feat_in[N_sparse * n_chan],
                  hash_T sparse_arr_hash[N_sparse * 2],
                  w_T w[3 * 3 * n_chan * n_filt],
                  b_T b[n_filt]) {
-    // note the order of filter weights stored by hls4ml
-    // pixel_loop->filter_loop->channel_loop, so (h, w, f, c) fattened
-    // if 2 channels, 2 filters:
-    // [p1-f1-c1, p1-f1-c2, p1-f2-c1, p1-f2-c2, p2-f1-c1,...]
+    // Note the ordering of filter weights stored by hls4ml,
+    // pixel_loop->filter_loop->channel_loop, so shape=(h, w, f, c) fattened.
+    // If there are two input channels and two output filters:
+    // [p1-f1-c1, p1-f1-c2, p1-f2-c1, p1-f2-c2, p2-f1-c1,...].
 
+    // For sparse convolution, we assume the same sparsity structure between input and output,
+    // which reduces computing complexity and avoid the dilation problem.
+    // So the sparse_arr_hash stays exactly the same, we just compute the new feature values for the same set of pixel locations.
+
+    // Loop over output pixels (same set of input pixels).
     OutputPixelLoop:
-    for (int i_out = 0; i_out < N_sparse; i_out++) {
+    for (int i_pixel_out = 0; i_pixel_out < N_sparse; i_pixel_out++) {
         #pragma HLS UNROLL
 
+        // Loop over output filters.
         OutputFilterLoop:
         for (int i_filt = 0; i_filt < n_filt; i_filt++) {
             #pragma HLS UNROLL
             res_T acc = 0;
 
-            // central field per input channel per output filter
+            // Multiplication for the central pixel, which is always happening without checking the offsets.
             InputChannelLoopForCentralField:
             for (int i_chan = 0; i_chan < n_chan; i_chan++) {
                 #pragma HLS UNROLL
-                acc += sparse_arr_feat_in[n_chan * i_out + i_chan] * w[4 * n_chan * n_filt + n_chan * i_filt + i_chan];
+                acc += sparse_arr_feat_in[n_chan * i_pixel_out + i_chan] * w[4 * n_chan * n_filt + n_chan * i_filt + i_chan];
             }
         
-            // look for fields away from the central by offset checking
+            // Loop over input pixels for the current output pixel.
             InputPixelLoop:
-            for (int i_in = 0; i_in < N_sparse; i_in++) {
+            for (int i_pixel_in = 0; i_pixel_in < N_sparse; i_pixel_in++) {
                 #pragma HLS UNROLL
 
-                int offset_h = sparse_arr_hash[2 * i_out] - sparse_arr_hash[2 * i_in];
-                int offset_w = sparse_arr_hash[2 * i_out + 1] - sparse_arr_hash[2 * i_in + 1];
+                // Compute the relative positions between the output and input pixels in height and width.
+                int offset_h = sparse_arr_hash[2 * i_pixel_out] - sparse_arr_hash[2 * i_pixel_in];
+                int offset_w = sparse_arr_hash[2 * i_pixel_out + 1] - sparse_arr_hash[2 * i_pixel_in + 1];
 
-                acc += mult_for_sparse_conv<data_T, res_T, w_T, n_chan, n_filt, N_sparse>(offset_h, offset_w, sparse_arr_feat_in, w, i_filt, i_in);
+                acc += mult_for_sparse_conv_kernel3<data_T, res_T, w_T, n_chan, n_filt, N_sparse>(offset_h, offset_w, sparse_arr_feat_in, w, i_filt, i_pixel_in);
             }
-            sparse_arr_feat_out[n_filt * i_out + i_filt] = acc + b[i_filt];
+
+            // Add bias.
+            acc += b[i_filt];
+            sparse_arr_feat_out[n_filt * i_pixel_out + i_filt] = acc;
         }
     }
 }
@@ -176,9 +209,12 @@ void sparse_pooling_avg(data_T sparse_arr_feat_in[N_sparse * n_chan],
                         res_T sparse_arr_feat_out[N_sparse * n_chan],
                         hash_T sparse_arr_hash_in[N_sparse * 2],
                         hash_T sparse_arr_hash_out[N_sparse * 2]) {
-    int hash_tmp[N_sparse * 2];
-    #pragma HLS ARRAY_PARTITION variable=hash_tmp type=complete dim=0
+    // This function does average pooling on sparse arrays.
+    // It first computes the pooled positions for all active pixels stored in the sparse arrays,
+    // then collects pixels falling in the same pool and computes the average per pool.
+    // This therefore gives new sparse_arr_hash and sparse_arr_feat at outputs.
 
+    // Store the 1/pool_size value instead of doing division at runtime.
     ap_fixed<10,0> pool_size_recip = 0;
     if (pool_size == 2) { pool_size_recip = 0.5; }
     else if (pool_size == 3) { pool_size_recip = 0.33333; }
@@ -186,54 +222,75 @@ void sparse_pooling_avg(data_T sparse_arr_feat_in[N_sparse * n_chan],
     else if (pool_size == 5) { pool_size_recip = 0.2; }
     else if (pool_size == 6) { pool_size_recip = 0.16667; }
 
-    ComputePoolCoord:
+    // First compute the pooled postitions for all pixels.
+    // This is needed here separately first since we need to do nested loops over it later.
+    int hash_tmp[N_sparse * 2];
+    #pragma HLS ARRAY_PARTITION variable=hash_tmp type=complete dim=0
+    ComputePooledLoc:
     for (int i = 0; i < N_sparse; i++) {
         #pragma HLS UNROLL
         hash_tmp[2 * i] = (sparse_arr_hash_in[2 * i] - 1) * pool_size_recip + 1;
         hash_tmp[2 * i + 1] = (sparse_arr_hash_in[2 * i + 1] - 1) * pool_size_recip + 1;
     }
 
-    data_T sparse_arr_feat_in_local[N_sparse * n_chan];
-    #pragma HLS ARRAY_PARTITION variable=sparse_arr_feat_in_local type=complete dim=0
-    for (int ii = 0; ii < N_sparse * n_chan; ii++) {
+    // Get a copy of the sparse feature array,
+    // as we will need to use it to zero out pixels later for keeping track of when the pooling is done on a pixel to avoid double counting.
+    data_T sparse_arr_feat_in_copy[N_sparse * n_chan];
+    #pragma HLS ARRAY_PARTITION variable=sparse_arr_feat_in_copy type=complete dim=0
+    for (int i = 0; i < N_sparse * n_chan; i++) {
         #pragma HLS UNROLL
-        sparse_arr_feat_in_local[ii] = sparse_arr_feat_in[ii];
+        sparse_arr_feat_in_copy[i] = sparse_arr_feat_in[i];
     }
 
+    // Now, the pooling operation.
+    // Loop over the pixels.
     HashOutLoop:
-    for (int i = 0; i < N_sparse; i++) {
+    for (int i_pixel_out = 0; i_pixel_out < N_sparse; i_pixel_out++) {
         #pragma HLS UNROLL
-        int i_h_out = hash_tmp[2 * i];
-        int i_w_out = hash_tmp[2 * i + 1];
+        // Get the pooled locations for a given output hash.
+        int h_out = hash_tmp[2 * i_pixel_out];
+        int w_out = hash_tmp[2 * i_pixel_out + 1];
 
+        // Loop over the input channels.
         ChannelLoop:
         for (int i_chan = 0; i_chan < n_chan; i_chan++) {
             #pragma HLS UNROLL
             res_T acc = 0;
 
+            // Loop over given the pixels to find if any belongs to the same pool for a given i_pixel_out.
             HashInLoop:
-            for (int j = 0; j < N_sparse; j++) {
+            for (int i_pixel_in = 0; i_pixel_in < N_sparse; i_pixel_in++) {
                 #pragma HLS UNROLL
-                int j_h_out = hash_tmp[2 * j];
-                int j_w_out = hash_tmp[2 * j + 1];
+                // Get the pooled locations for a given input hash.
+                int h_in = hash_tmp[2 * i_pixel_in];
+                int w_in = hash_tmp[2 * i_pixel_in + 1];
 
-                data_T data = sparse_arr_feat_in_local[n_chan * j + i_chan];
-                if ((i_h_out == j_h_out) && (i_w_out == j_w_out)) {
+                // Accumulate if the i_pixel_in belong to the same pool as i_pixel_out.
+                data_T data = sparse_arr_feat_in_copy[n_chan * i_pixel_in + i_chan];
+                if ((h_out == h_in) && (w_out == w_in)) {
                     acc += data;
-                    sparse_arr_feat_in_local[n_chan * j + i_chan] = 0;
+                    // Zero it out so the next iteration will not double count it.
+                    sparse_arr_feat_in_copy[n_chan * i_pixel_in + i_chan] = 0;
                 }
             }
-            sparse_arr_feat_out[n_chan * i + i_chan] = acc * pool_size_recip * pool_size_recip;
+            // Divide the sum by the number pixels within the pool.
+            sparse_arr_feat_out[n_chan * i_pixel_out + i_chan] = acc * pool_size_recip * pool_size_recip;
         }
-        sparse_arr_hash_out[2 * i] = i_h_out;
-        sparse_arr_hash_out[2 * i + 1] = i_w_out;
+        // Fill the pixel locations after pooling.
+        sparse_arr_hash_out[2 * i_pixel_out] = h_out;
+        sparse_arr_hash_out[2 * i_pixel_out + 1] = w_out;
     }
+    // Note that there could be more zero-padded elements in sparse_arr_feat_out than sparse_arr_feat_in due to pooling,
+    // and the new zero-padded elements will have redundant values in hash.
+    // But whenever the element is zero-padded in the sparse feat array, it will be ignored by sparse operations no matter what its hash is.
 }
 
 template<class data_T, class hash_T, int n_height, int n_width, int n_chan, int N_sparse>
 void sparse_flatten(data_T sparse_arr_feat[N_sparse * n_chan],
                     hash_T sparse_arr_hash[N_sparse * 2],
                     data_T flat_arr[n_height * n_width * n_chan]) {
+    // This function does full flattening on sparse arrays.
+    
     InitFlatArr:
     for (int i = 0; i < n_height * n_width * n_chan; i++) {
         #pragma HLS UNROLL
@@ -243,14 +300,19 @@ void sparse_flatten(data_T sparse_arr_feat[N_sparse * n_chan],
     FillFlatArr:
     for (int i = 0; i < N_sparse; i++) {
         #pragma HLS UNROLL
-        int j_h = sparse_arr_hash[2 * i];
-        int j_w = sparse_arr_hash[2 * i + 1];
-        int pixel_idx = (j_h - 1) * n_width + (j_w - 1);
+        // Compute the pixel number from its height and width indices.
+        int i_h = sparse_arr_hash[2 * i];
+        int i_w = sparse_arr_hash[2 * i + 1];
+        int pixel_idx = (i_h - 1) * n_width + (i_w - 1);
 
+        ChannelLoop:
         for (int i_chan = 0; i_chan < n_chan; i_chan++) {
             #pragma HLS UNROLL
             data_T data = sparse_arr_feat[n_chan * i + i_chan];
-            if (data != 0) { // this check is needed as the hash arr after pooling could have duplicate pixels with 0 feat val
+
+            // This check is needed as the hash array after pooling could have duplicate pixels with 0 feat val,
+            // so it avoids over-writing the duplicate pixels with zeros.
+            if (data != 0) {
                 flat_arr[n_chan * pixel_idx + i_chan] = data;
             }
         }
